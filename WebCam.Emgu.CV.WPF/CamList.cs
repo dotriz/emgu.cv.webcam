bbd9bf8f0976c59b7426d32308d97c087ec762f3
Rizwan <grizwan@gmail.com>
2020-01-17 20:14:10 +0500

    Camera video display added using Emgu along with Camera List

---
+++ WebCam.Emgu.CV.WPF/CamList.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace WebCam_Emgu_CV_WPF
+{
+    //Source: https://sourceforge.net/projects/csharp-web-cam-list/
+    public class CamList
+    {
+        internal static readonly Guid SystemDeviceEnum = new Guid(0x62BE5D10, 0x60EB, 0x11D0, 0xBD, 0x3B, 0x00, 0xA0, 0xC9, 0x11, 0xCE, 0x86);
+        internal static readonly Guid VideoInputDevice = new Guid(0x860BB310, 0x5D01, 0x11D0, 0xBD, 0x3B, 0x00, 0xA0, 0xC9, 0x11, 0xCE, 0x86);
+
+        [ComImport, Guid("55272A00-42CB-11CE-8135-00AA004BB851"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        internal interface IPropertyBag
+        {
+            [PreserveSig]
+            int Read(
+                [In, MarshalAs(UnmanagedType.LPWStr)] string propertyName,
+                [In, Out, MarshalAs(UnmanagedType.Struct)] ref object pVar,
+                [In] IntPtr pErrorLog);
+            [PreserveSig]
+            int Write(
+                [In, MarshalAs(UnmanagedType.LPWStr)] string propertyName,
+                [In, MarshalAs(UnmanagedType.Struct)] ref object pVar);
+        }
+
+        [ComImport, Guid("29840822-5B84-11D0-BD3B-00A0C911CE86"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+        internal interface ICreateDevEnum
+        {
+            [PreserveSig]
+            int CreateClassEnumerator([In] ref Guid type, [Out] out IEnumMoniker enumMoniker, [In] int flags);
+        }
+
+        public class Device
+        {
+            public int Index { get; set; }
+            public IMoniker Mon { get; set; }
+            public string Name { get; set; }
+            public string DevicePath { get; set; }
+            public Guid ClassID { get; set; }
+
+            public Device(int index, string name, IMoniker mon, string devicePath, Guid classID)
+            {
+                this.Index = index;
+                this.Name = name;
+                this.ClassID = classID;
+                this.Mon = mon;
+                this.DevicePath = devicePath;
+            }
+
+            public Device() { }
+        }
+
+        public static List<Device> FindCameras()
+        {
+            List<Device> devices = new List<Device>();
+
+            Object bagObj = null;
+            object comObj = null;
+            ICreateDevEnum enumDev = null;
+            IEnumMoniker enumMon = null;
+            IMoniker[] moniker = new IMoniker[100];
+            IPropertyBag bag = null;
+            try
+            {
+                // Get the system device enumerator
+                Type srvType = Type.GetTypeFromCLSID(SystemDeviceEnum);
+                // create device enumerator
+                comObj = Activator.CreateInstance(srvType);
+                enumDev = (ICreateDevEnum)comObj;
+                // Create an enumerator to find filters of specified category
+                enumDev.CreateClassEnumerator(VideoInputDevice, out enumMon, 0);
+                Guid bagId = typeof(IPropertyBag).GUID;
+                int index = 0;
+                while (enumMon.Next(1, moniker, IntPtr.Zero) == 0)
+                {
+                    // get property bag of the moniker
+                    moniker[0].BindToStorage(null, null, ref bagId, out bagObj);
+                    bag = (IPropertyBag)bagObj;
+                    // read FriendlyName
+                    object friendlyName = "";
+                    object devicePath = "";
+                    object classID = "";
+                    bag.Read("FriendlyName", ref friendlyName, IntPtr.Zero);
+                    bag.Read("DevicePath", ref devicePath, IntPtr.Zero);
+                    bag.Read("ClassID", ref classID, IntPtr.Zero);
+                    //list in box
+                    devices.Add(
+                        new Device()
+                        {
+                            Index = index,
+                            Name = (string)friendlyName,
+                            DevicePath = (string)devicePath
+                            //,
+                            //ClassID = (Guid)classID
+                        }
+                   );
+                    index++;
+                }
+
+            }
+            catch (Exception)
+            {
+            }
+            finally
+            {
+                bag = null;
+                if (bagObj != null)
+                {
+                    Marshal.ReleaseComObject(bagObj);
+                    bagObj = null;
+                }
+                enumDev = null;
+                enumMon = null;
+                moniker = null;
+            }
+
+            return devices;
+        }
+    }
+}
+
+
+
+
+
---
+++ WebCam.Emgu.CV.WPF/MainWindow.xaml.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Emgu.CV;
+using System.IO;
+using System.Threading;
+using DirectShowLib;
+using System.Collections.ObjectModel;
+using System.Runtime.InteropServices.ComTypes;
+using System.Collections.Generic;
+
+namespace WebCam_Emgu_CV_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private VideoCapture _capture = null;
+        private Mat _frame = new Mat();
+
+        #region Public properties
+
+        public ObservableCollection<Filter_Info> VideoDevices { get; set; }
+
+        public Filter_Info CurrentDevice
+        {
+            get { return _currentDevice; }
+            set { _currentDevice = value; this.OnPropertyChanged("CurrentDevice"); }
+        }
+        private Filter_Info _currentDevice;
+
+        #endregion
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Closing += MainWindow_Closing;
+            this.DataContext = this;
+            GetVideoDevices();
+        }
+
+        private void GetVideoDevices()
+        {
+            //-> Find systems cameras with DirectShow.Net dll
+            //DsDevice[] _SystemCamereas = DsDevice.GetDevicesOfCat(FilterCategory.VideoInputDevice);
+            //VideoDevices = new ObservableCollection<Filter_Info>();
+            //for (int i = 0; i < _SystemCamereas.Length; i++)
+            //{
+            //    Video_Device WebCam = new Video_Device() {
+            //        Index = i,
+            //        Name = _SystemCamereas[i].Name,
+            //        Mon = _SystemCamereas[i].Mon,
+            //        DevicePath = _SystemCamereas[i].DevicePath,
+            //        ClassID = _SystemCamereas[i].ClassID
+            //    };
+
+            //    Filter_Info fi = new Filter_Info
+            //    {
+            //        Index = i,
+            //        MonikerString = WebCam.Mon,
+            //        Name = WebCam.Name
+            //    };
+
+            //    VideoDevices.Add(fi);
+            //}
+
+            VideoDevices = new ObservableCollection<Filter_Info>();
+            List<CamList.Device> devices =  CamList.FindCameras();
+            foreach(var device in devices)
+            {
+
+                Filter_Info fi = new Filter_Info
+                {
+                    Index = device.Index,
+                    MonikerString = device.Mon,
+                    Name = device.Name
+                };
+
+                VideoDevices.Add(fi);
+            }
+
+            if (VideoDevices.Any())
+            {
+                CurrentDevice = VideoDevices[0];
+            }
+            else
+            {
+                MessageBox.Show("No video sources found", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void MainWindow_Closing(object sender, CancelEventArgs e)
+        {
+            StopCamera();
+        }
+
+        private void ProcessFrame(object sender, EventArgs arg)
+        {
+            if (_capture != null && _capture.Ptr != IntPtr.Zero)
+            {
+                _capture.Retrieve(_frame, 0);
+
+                Dispatcher.BeginInvoke(new ThreadStart(delegate
+                {
+                    videoPlayer.Source = ConvertBitmap(_frame.Bitmap);
+                }));
+            }
+        }
+
+        private void StartCamera()
+        {
+            if (CurrentDevice != null)
+            {
+                _capture = new VideoCapture(CurrentDevice.Index);
+                _capture.ImageGrabbed += ProcessFrame;
+                //_capture.FlipHorizontal = true;
+                _capture.Start();
+            }
+        }
+
+        private void StopCamera()
+        {
+            if (_capture != null && _capture.Ptr != IntPtr.Zero)
+            {
+                _capture.Stop();
+                _capture.Dispose();
+                _capture.ImageGrabbed -= ProcessFrame;
+            }
+        }
+
+        public BitmapImage ConvertBitmap(System.Drawing.Bitmap bitmap)
+        {
+            MemoryStream ms = new MemoryStream();
+            bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+            BitmapImage image = new BitmapImage();
+            image.BeginInit();
+            ms.Seek(0, SeekOrigin.Begin);
+            image.StreamSource = ms;
+            image.EndInit();
+
+            return image;
+        }
+
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            StartCamera();
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            StopCamera();
+        }
+
+        #region INotifyPropertyChanged members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                var e = new PropertyChangedEventArgs(propertyName);
+                handler(this, e);
+            }
+        }
+
+        #endregion
+    }
+
+    public class Filter_Info
+    {
+        public int Index { get; set; }
+        public string Name { get; set; }
+        public IMoniker MonikerString { get; set; }
+    }
+
+    internal class Video_Device
+    {
+        public int Index { get; set; }
+        public IMoniker Mon { get; set; }
+        public string Name { get; set; }
+        public string DevicePath { get; set; }
+        public Guid ClassID { get; set; }
+
+        public Video_Device(int index, string name, IMoniker mon, string devicePath, Guid classID)
+        {
+            this.Index = index;
+            this.Name = name;
+            this.ClassID = classID;
+            this.Mon = mon;
+            this.DevicePath = devicePath;
+        }
+
+        public Video_Device(){ }
+    }
+
+}
